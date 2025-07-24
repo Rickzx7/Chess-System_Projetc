6e6edf3a7bccab8dd55f5cf7ad3419fa37d61a17
Rickzx7 <ricardoaraujocandido516@gmail.com>
2025-07-24 19:35:30 -0300

    Added: King and Rook pieces

---
+++ src/Pieces/King.java
@@ -0,0 +1,17 @@
+package Pieces;
+
+import Chess.ChessPiece;
+import Chess.Color;
+import boardgame.Board;
+import boardgame.Piece;
+
+public class King extends ChessPiece {
+    public King(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString() {
+        return "K";
+    }
+}
