61c0e4d64404f3580d3ca51794b450823ac8c455
Rickzx7 <ricardoaraujocandido516@gmail.com>
2025-07-25 15:39:36 -0300

    Added: color in possible moves pieces

--- src/application/Main.java
+++ src/application/Main.java
@@ -24,9 +24,14 @@ public class Main {
                 System.out.println("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
 
+                boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces(), possibleMoves);
+
                 System.out.println();
                 System.out.println("target: ");
                 ChessPosition target = UI.readChessPosition(sc);
+
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
             }
             catch (ChessExeception | InputMismatchException e) {
