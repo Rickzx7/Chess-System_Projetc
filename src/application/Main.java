a9860d77b9765096364085915a9c7f0c0a6fe59a
Rickzx7 <ricardoaraujocandido516@gmail.com>
2025-07-29 10:25:55 -0300

    add special move: Promotion

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,6 +8,7 @@ import boardgame.Board;
 import boardgame.BoardExeception;
 import boardgame.Position;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -41,6 +42,12 @@ public class Main {
 
                 if(capturedPiece != null){
                     captured.add(capturedPiece);
+                }
+
+                if(chessMatch.getPromoted() !=null){
+                    System.out.println("Enter piece for promotion (B/N/R/Q)");
+                    String type = sc.nextLine();
+                    chessMatch.replacePromotedPiece(type);
                 }
             }
             catch (ChessExeception | InputMismatchException e) {
